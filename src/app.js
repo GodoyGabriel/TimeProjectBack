49c3fb6b1ad4f23dff50abf1d3b78a9ee15c3a8a
Gabriel Godoy <godoygabriel95@hotmail.com>
2021-05-04 21:57:00 -0300

    Add CORS

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const timeRoutes = require('./routes/timeRoutes');
 const metricsRoutes = require('./routes/metricsRoutes');
+const cors = require('cors');
 
+app.use(cors());
 // Routes
 app.use('/', timeRoutes);
 app.use('/', metricsRoutes);
